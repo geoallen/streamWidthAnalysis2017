d8609c8a276fc10bfb79266b09498c0082767061
George Allen <georgehenryallen@gmail.com>
2017-10-20 12:30:09 -0400

    Update smallStreamsAnalysis.R

--- smallStreamsAnalysis.R
+++ smallStreamsAnalysis.R
@@ -1,21 +1,22 @@
-# smallStreamsAnalysis.R
-# George Allen, Feb 2017  
+# allen-headwater-stream-widths-distributions-study
 
-# contains R code used to analyze the data and create figures and tables
-# presented in Allen et al., "Similarity of stream width distributions in headwater catchments" 
+# contains R code used to analyze the data presented in Allen et al., "Similarity of stream width distributions in headwater catchments" 
 
 # to run:
-# download all data and R scripts into the same working directory
+# download all data a scripts into the same working directory
 # open top level R script: "smallStreamsAnalysis.R" 
-# specify workingDir as your working directory path (below)
-# select and run codes to reproduce figures and analysis
+# specify workingDir to your working directory path
+#run code to reproduce figures and analysis
+  
+# fieldwork_analysis.R
+# George Allen, Oct 2017  
 
 #############################################################################################
-# Define input parameters. This section contains pointers 
-# and constants to run scripts:
+# Define input parameters:  
 
+# contains pointers to run:
 
-workingDir = 'E:/misc/2015_09_01_Small_Stream_Width_Fieldwork'
+workingDir = 'path/to/your/working/directory'
 
 # data table file names:
 fNames = c('konza', 'sagehen', 'angelo', 'caribou', 'v40', 'blueduck', 'stony',
@@ -31,18 +32,13 @@ tabNames = c('Kings', 'Sagehen', 'Elder', "Caribou", "V40", "Blue Duck", "Stony"
              "2015-10-27", "2015-12-09", "2016-02-02",
              "2016-02-14", "2016-03-04a", "2016-03-04b")
 
-
 #############################################################################################
-# this section loads codes used to produce figures in the main manuscript:
-
 # Figure Scripts:
 
-# Figure 1 was created using ArcMap. 
-
-# Alternative Fig. 1 - stream width map generator:
-source(paste0(workingDir, '/R/Fig1_widthMap.R'))
-pdfOut = paste0(workingDir, '/figures/Fig1_widthMap.pdf')
-Fig1_widthMap(inTabPaths, tabNames, pdfOut)
+# Figure 1 - stream width map generator:
+source(paste0(workingDir, '/R/widthMap.R'))
+pdfOut = paste0(workingDir, '/figures/widthMap.pdf')
+widthMap(inTabPaths, tabNames, pdfOut)
 
 # Figure 2 - stream width distributions:
 source(paste0(workingDir, '/R/fig2_distributions.R'))
@@ -52,8 +48,8 @@ fig2_distributions(inTabPaths, tabNames, pdfOut)
 # Figure 3 - modeled stream widths:
 source(paste0(workingDir, '/R/fig3_widthModel.R'))
 pdfOut = paste0(workingDir, '/figures/fig3_widthModel4_3.pdf')
-csvOut = paste0(workingDir, '/tables/modeledWidthTab4_3.csv')
-fig3_widthModel(inTabPaths[c(1,2,3,6,7)], csvOut, pdfOut)
+csvOut = paste0(workingDir, '/tables/modeledWidthTab4_3')
+fig3_widthModel(inTabPaths, tabNames, csvOut, pdfOut)
 
 # Figure 4 was produced in Adobe Illustrator
 
@@ -64,17 +60,20 @@ fig3_widthModel(inTabPaths[c(1,2,3,6,7)], csvOut, pdfOut)
 # ED Figure 1 was produced in Adobe Illustrator 
 
 # ED table 1 and table 2 - catchment attributes:
-source(paste0(workingDir, '/R/EDtab1_EDtab2_catchment_attributes.R'))
-csvOut = paste0(workingDir, '/tables/EDtab1_EDtab2.csv')
-EDtab1_EDtab2_catchment_attributes(inTabPaths, tabNames, csvOut, workingDir)
+source(paste0(workingDir, '/R/EDtable_catchment_attributes.R'))
+csvOut = paste0(workingDir, '/tables/EDtable1.csv')
+EDtable_catchment_attributes(inTabPaths, tabNames, csvOut, workingDir)
 
 # ED Figure 2 and ED Table 3 - quantify GOF for distributions:
-source(paste0(workingDir, '/R/ED_fig2_tab3_GOF.R'))
-pdfOut = paste0(workingDir, '/figures/ED_fig2_GOF.pdf')
+source(paste0(workingDir, '/R/EDfig2_EDtab3_GOF.R'))
+modTabDir = paste0(workingDir, '/tables')
+pdfOut = paste0(workingDir, '/figures/EDfig2_GOF.pdf')
 csvOut = paste0(workingDir, '/tables/EDtable3_GOF.csv')
-ED_fig2_tab3_GOF(inTabPaths, tabNames, pdfOut, csvOut)
+EDfig2_EDtab3_GOF(inTabPaths, modTabDir, tabNames, pdfOut, csvOut)
 
-# ED table 4 - CO2 efflux calculation:
-source(paste0(workingDir, '/R/EDtab4_co2_flux.R'))
-csvOut = paste0(workingDir, '/tables/EDtab4_co2_flux.csv')
-EDtab4_co2_flux(inTabPaths, tabNames, csvOut, workingDir)
+# ED table 4 - efflux calculation:
+source(paste0(workingDir, '/R/EDtable3.R'))
+csvOut = paste0(workingDir, '/tables/EDtable3.csv')
+EDtable4(inTabPaths, tabNames, csvOut, workingDir)
+
+
